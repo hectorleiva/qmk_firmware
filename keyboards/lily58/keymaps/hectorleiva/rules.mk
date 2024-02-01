ebde657306389fff4e09e8a115907563c719c5f5
Hector Leiva <hector@hectorleiva.com>
2024-01-31 23:10:14 -0500

    adding and writing the eduardo_logo to the lily58/hectorleiva keyboard. I hate what VSCode did to reformat all the C that was there

--- keyboards/lily58/keymaps/hectorleiva/rules.mk
+++ keyboards/lily58/keymaps/hectorleiva/rules.mk
@@ -16,8 +16,8 @@ VIA_ENABLE = yes            # VIA support
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
-        ./lib/logo_reader.c \
         ./lib/keylogger.c \
-        ./lib/timelogger.c \
+        # ./lib/logo_reader.c \ # OG lily58 logo
+        # ./lib/timelogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
